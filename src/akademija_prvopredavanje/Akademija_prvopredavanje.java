e6619f493a79b8f8becabea1afb0de51f955d576
Ana Tasic <tasicana996@gmail.com>
2017-12-19 03:19:51 +0100

    Initial commit.

---
+++ src/akademija_prvopredavanje/Akademija_prvopredavanje.java
@@ -0,0 +1,11 @@
+
+package akademija_prvopredavanje;
+
+public class Akademija_prvopredavanje {
+
+    public static void main(String[] args) {
+        System.out.println("CAOOO");
+    }
+    
+} 
+
